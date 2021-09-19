9d30c2fa56672d11c8ad69167966c96bdc1ee596
Brian Grosso <bgro63@gmail.com>
2021-09-19 19:18:46 -0400

    hatchat init

--- client/App.js
+++ client/App.js
@@ -1,128 +1,17 @@
 import React, { Component, useState } from 'react';
-import Landing from './components/Landing';
-import JobDetails from './components/JobDetails.js';
-import JobModal from './components/JobModal';
-import Dashboard from './components/Dashboard.js';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect
-} from "react-router-dom";
-import { useSelector, useDispatch } from 'react-redux';
-import { GoogleLogin } from 'react-google-login';
 import axios from 'axios';
-import * as types from './constants/actionTypes';
 
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     // this.state = {
-//     //   loggedIn: false,
-//     // }
-//     this.responseGoogle = this.responseGoogle.bind(this);
-//   }
 const App = (props) => {
-  const [addingJob, setAddingJob] = useState(false);
-  const loggedIn = useSelector((state)=>state.user.loggedIn);
-  const editMode = useSelector((state)=>state.job.editMode);
-  const dispatch = useDispatch();
-  function responseGoogle (response) {
-    axios.post('/auth/google', {
-      firstName: response.Rs.mU,
-      lastName: response.Rs.mS,
-      email: response.Rs.Ct,
-      token: response.Rs.$I,
-    })
-    .then((res) => {
-      dispatch({
-        type: types.USER_LOGGED_IN,
-        payload: {
-          firstName: response.Rs.mU,
-          lastName: response.Rs.mS,
-          createdAt: res.data.createdAt,
-          id: res.data.id,
-          jobs: res.data.jobs
-        } 
-      })
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  };
-
-  const jobs = useSelector((state)=>state.user.jobs);
-  const createdAt = useSelector(state => state.user.createdAt);
-  let dayCount = Math.ceil((Date.now()-Date.parse(createdAt))/(1000*60*60*24))
     return (
-      <div>
-        <Router>
-          <div id='navbar'>
-            <div id='leftbuttons'>
-            {loggedIn ? (
-                  <div id="daycount">
-                      {`Job hunting day: ${dayCount}`}
-                  </div>
-                ) : (
-                  <GoogleLogin
-                    clientId='351358931211-av32gv56l0qja9hrs47hi1va0j3uv4as.apps.googleusercontent.com'
-                    render={renderProps => (
-                    <button onClick={renderProps.onClick} disabled={renderProps.disabled}>Login</button>
-                    )}  
-                    buttonText="Login"
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                  />
-                )
-              }
-              {loggedIn ? (
-                  <button onClick={()=> setAddingJob(true)}> Add Job</button>
-                ) : (
-                  <GoogleLogin
-                    clientId='351358931211-av32gv56l0qja9hrs47hi1va0j3uv4as.apps.googleusercontent.com'
-                    render={renderProps => (
-                    <button onClick={renderProps.onClick} disabled={renderProps.disabled}>Sign Up</button>
-                    )}  
-                    buttonText="Sign Up"
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                  />
-                )
-              }
-            </div>
-            <Link to="/">
-            <img id="logo" src="../Crown_Vintage_Logo_no_Scroll.png"/>
-            </Link>
-            <div id="rightbuttons">
-            <button
-              id="linkedin"
-              onClick={(e) => {
-                e.preventDefault();
-                window.open("https://www.linkedin.com", "_blank") ||
-                  window.location.replace("https://www.linkedin.com");
-              }}
-            >
-              LinkedIn
-            </button>
-            <Link to="/team">
-              <button>Team</button>
-            </Link>
-          </div>
-          </div>
-          <Switch>
-            <Route exact path="/">
-              {addingJob || editMode ? (<JobDetails setAddingJob={setAddingJob} />) : (
-                loggedIn ? (
-                  <Dashboard />
-                  ) : <Landing responseGoogle={responseGoogle}/> 
-                )
-              }
-            </Route>
-          </Switch>
-        </Router>
+      <div id="app">
+        <div id="toprow">
+        <button id="start">Start a Call</button>
+        <img id="logo" src="./logo.gif" alt="logo"/>
+        <button>Join a Call</button>
+        </div>
+        <div id="bottomrow">
+          <input type="text" placeholder="Enter Call ID if joining" />
+        </div>
       </div>
     )
 }
--- client/index.js
+++ client/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './store';
 
 // uncomment so that webpack can bundle styles
 import styles from './scss/application.scss';
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+render(<App />,document.getElementById('root'));
