0296d83ce24c9b9752cb9c4e0e88b6bbe811b58f
Brian Grosso <bgro63@gmail.com>
2021-09-19 23:43:00 -0400

    google stun server2

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer({
     config: {'iceServers': [
-      { url: 'stun:stun.l.google.com:19302' },
+      { url: 'stun:stun.l.google.com:19302' }//,
     //   {url:'stun:stun1.l.google.com:19302'},
     //   {url:'stun:stun2.l.google.com:19302'},
     //   {url:'stun:stun3.l.google.com:19302'},
